3c46dfff5b7500c569d6fbcdc6bac03fe1bef0bb
bregarulai <bregarulai@gmail.com>
2022-05-30 23:20:40 -0400

    created structure for home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import {
+  Featured,
+  FeaturedProperties,
+  Footer,
+  Header,
+  Maillist,
+  Navbar,
+  PropertyList,
+} from '../../components';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <div className='home__container'>
+        <Featured />
+        <h1 className='home__title'>Browse by property type</h1>
+        <PropertyList />
+        <h1 className='home__title'>Home guests love</h1>
+        <FeaturedProperties />
+        <Maillist />
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
