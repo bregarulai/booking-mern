04a149f2dfd96a1ba398b7dbbbc490807e7221de
bregarulai <bregarulai@gmail.com>
2022-05-30 23:54:01 -0400

    created header top section jsx

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBed,
+  faCar,
+  faPlane,
+  faTaxi,
+} from '@fortawesome/free-solid-svg-icons';
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ type }) => {
+  return (
+    <div className='header'>
+      <div
+        className={
+          type === 'hotels'
+            ? 'header__container header__container--hotels'
+            : 'header__container'
+        }
+      >
+        <div className='header__list'>
+          <div className='header__item'>
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className='header__item'>
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className='header__item'>
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car Rentals</span>
+          </div>
+          <div className='header__item'>
+            <FontAwesomeIcon icon={faBed} />
+            <span>Acttractions</span>
+          </div>
+          <div className='header__item'>
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport Taxis</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
