0e88f8613bef41b3414608a18fc23c4e34b4249e
bregarulai <bregarulai@gmail.com>
2022-05-30 23:29:57 -0400

    created navbar initial jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='navbar'>
+      <div className='navbar__container'>
+        <Link className='navbar__logo' to='/'>
+          BregaBooking
+        </Link>
+        <div className='navbar__items'>
+          <button className='navbar__button'>Register</button>
+          <button className='navbar__button'>Login</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
